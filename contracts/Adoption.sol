c72e2076e31816650c1920175321ebc968389aa4
Elemino <elemshalit@gmail.com>
2018-11-29 00:21:14 -0800

    Enclose functions in contract and compile

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -3,8 +3,6 @@ pragma solidity ^0.4.17;
 contract Adoption {
     address[16] public adopters;
 
-}
-
 // Adopting a pet
 function adopt(uint petId) public returns (uint) {
   require(petId >= 0 && petId <= 15);
@@ -14,7 +12,11 @@ function adopt(uint petId) public returns (uint) {
   return petId;
 }
 
+
 // Retrieving the adopters
 function getAdopters() public view returns (address[16]) {
   return adopters;
 }
+
+}
+
